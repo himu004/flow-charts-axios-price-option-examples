7cab1a8de3bf220bc75df695f5930af42d6c8e0b
Himu004 <himu.chowdhury01@gmail.com>
2023-09-21 16:24:25 +0600

    Spinner Added

--- src/Phones/Phones.jsx
+++ src/Phones/Phones.jsx
@@ -2,15 +2,18 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import React, { PureComponent } from 'react';
 import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import { Blocks } from 'react-loader-spinner';
 
 const Phones = () => {
     const [phones, setPhones] = useState([]);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         // fetch('https://openapi.programming-hero.com/api/phones?search=iphone')
         // .then(res => res.json())
         // .then(data => setPhones(data.data))
 
+        // Data Fetch Using AXIOS 
         axios.get('https://openapi.programming-hero.com/api/phones?search=iphone')
         .then(data => {
             const phoneData = data.data.data;
@@ -22,11 +25,23 @@ const Phones = () => {
                 return obj;
             })
             setPhones(withFakeData);
+            setLoading(false);
         });
         
     },[]);
     return (
         <div>
+            {loading && 
+            <div>
+                <Blocks
+                    visible={true}
+                    height="80"
+                    width="80"
+                    ariaLabel="blocks-loading"
+                    wrapperStyle={{}}
+                    wrapperClass="blocks-wrapper"
+                />
+            </div>}
             <h2 className="text-5xl">Phones: {phones.length}</h2>
             <BarChart width={1200} height={400} data={phones}>
                  <Bar dataKey="price" fill="#8884d8" />
